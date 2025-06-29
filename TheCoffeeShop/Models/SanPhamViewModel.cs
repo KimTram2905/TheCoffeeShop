2fd36556b2c37fdcadcde6853480a92aa434ce67
lhbtrn <lhbaotrn2004@gmail.com>
2025-06-29 04:33:57 -0700

    Add files via upload

---
+++ TheCoffeeShop/Models/SanPhamViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace QuanLyQuanCafe.Models
+{
+    public class SanPhamViewModel
+    {
+        public int MaSanPham { get; set; }
+        public string TenSanPham { get; set; }
+        public int MaDanhMuc { get; set; }
+        public decimal DonGia { get; set; }
+        public string MoTa { get; set; }
+
+        public IFormFile? AnhUpload { get; set; } // ảnh upload
+    }
+}
