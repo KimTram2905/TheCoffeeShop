5e0422a4581c31cb6f942a56a42d269dffc9c5b9
lhbtrn <lhbaotrn2004@gmail.com>
2025-06-29 09:53:27 -0700

    Add files via upload

---
+++ TheCoffeeShop/Controllers/ViewAsPdf.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheCoffeeShop.Models;
+
+namespace QuanLyQuanCafe.Controllers
+{
+    internal class ViewAsPdf : IActionResult
+    {
+        public ViewAsPdf(string v, HoaDon hoaDon)
+        {
+        }
+
+        public string FileName { get; set; }
+
+        public Task ExecuteResultAsync(ActionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
