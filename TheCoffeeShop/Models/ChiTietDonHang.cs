2b69a6b9b83dca416d6aaf7bc9827612f537fd3f
lhbtrn <lhbaotrn2004@gmail.com>
2025-06-29 09:47:35 -0700

    Add files via upload

---
+++ TheCoffeeShop/Models/ChiTietDonHang.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TheCoffeeShop.Models;
+
+public partial class ChiTietDonHang
+{
+    public int MaChiTiet { get; set; }
+
+    public int MaDonHang { get; set; }
+
+    public int MaSanPham { get; set; }
+
+    public int SoLuong { get; set; }
+
+    public decimal DonGia { get; set; }
+
+    public decimal? ThanhTien { get; set; }
+
+    public string? GhiChu { get; set; }
+
+    public virtual DonHang MaDonHangNavigation { get; set; } = null!;
+
+    public virtual SanPham MaSanPhamNavigation { get; set; } = null!;
+
+    [NotMapped]
+    public string? TenSanPham { get; set; }
+
+    [NotMapped]
+    public int? MaDanhMuc { get; set; }
+
+}
